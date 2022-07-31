740a3a94c2b896a7f68920a1b660fe0a98c0141e
bsef19a505 <bsef19a505@pucit.edu.pk>
2022-07-31 14:05:30 +0500

    Seek Bar for Volume

--- Audio Player Android App/app/src/main/java/com/example/audioplayermobileapp/MainActivity.java
+++ Audio Player Android App/app/src/main/java/com/example/audioplayermobileapp/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.audioplayermobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
     MediaPlayer mediaPlayer;
+    AudioManager audioManager;
     public void play(View view){
         mediaPlayer.start();
 
@@ -25,5 +30,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mediaPlayer=MediaPlayer.create(this,R.raw.music);
+
+        SeekBar seekVol=findViewById(R.id.VolBar);
+
+        audioManager= (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+        int maxVol=audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int curVol=audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        seekVol.setMax(maxVol);
+        seekVol.setProgress(curVol);
     }
 }
